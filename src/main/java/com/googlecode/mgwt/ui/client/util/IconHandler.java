280fb85803780ec8ed566d02c94f06353ee24ece
Lars Hallqvist <perfecticus@hotmail.com>
2015-06-23 16:50:30 +0200

    Also set minW/H-property of the images to ensure they are initially the right size and avoids them being resized by the browser.

--- src/main/java/com/googlecode/mgwt/ui/client/util/IconHandler.java
+++ src/main/java/com/googlecode/mgwt/ui/client/util/IconHandler.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2014 Daniel Kurka
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -58,6 +58,8 @@ public class IconHandler {
 			element.getStyle().setProperty("WebkitMaskBoxImage", "url(" + icon.getSafeUri().asString() + ")");
 			element.getStyle().setWidth(dimensions.width, Unit.PX);
 			element.getStyle().setHeight(dimensions.height, Unit.PX);
+			element.getStyle().setProperty("minWidth", dimensions.width, Unit.PX);
+			element.getStyle().setProperty("minHeight", dimensions.height, Unit.PX);
 			element.getStyle().setProperty("WebkitMaskSize", dimensions.width + "px, " + dimensions.height + "px");
 			element.getStyle().setBackgroundColor(color);
 		}
